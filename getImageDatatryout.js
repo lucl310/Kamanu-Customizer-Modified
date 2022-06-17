44bc7937c3085761959c21dde6f5116df74c247d
Luc <lucthehuman21@gmail.com>
2022-06-17 11:30:50 -1000

    Refactored Code
    Removed redundancies and shortened the overall length of the code

--- getImageDatatryout.js
+++ getImageDatatryout.js
@@ -1,6 +1,10 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var img = new Image()
+var deck = "Deck"
+var hull = "Hull"
+var ama = "Ama"
+
 
   // for seperate function of draw image only on canoe deck use   ctx.putImageData(imgData, 0, 0, 0, 165, 1000, 100);   ctx.putImageData(imgData, 0, 0, 0, 400, 1000, 50), first for top hull, second for side hull
 
@@ -14,7 +18,7 @@ window.onload = function colorChange() {
   img.onload = function () {
     ctx.drawImage(img, 50, 0);
     var imgData = ctx.getImageData(0, 0, c.width, c.height);
-    // invert colors
+    
     var i;
     for (i = 0; i < imgData.data.length; i += 4) {
       imgData.data[i] = 255 - imgData.data[i];
@@ -29,7 +33,7 @@ window.onload = function colorChange() {
 function clearClick() {
   var img = new Image()
   var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
+  
   var i;
   for (i = 0; i < imgData.data.length; i += 4) {
     imgData.data[i] = 171;
@@ -60,190 +64,31 @@ function singleClick() {
     ctx.drawImage(img, x-150, y-100, 300, 300);
 };
 
-function changeRed() {
+function changeColor(red, green, blue, placement){
   var x = event.clientX;
   var y = event.clientY; 
   var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
+  
   var i;
   for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 255;
-    imgData.data[i+1] = 0;
-    imgData.data[i+2] = 0;
+    imgData.data[i] = red;
+    imgData.data[i+1] = green;
+    imgData.data[i+2] = blue;
     imgData.data[i+3] = 255;
   };
-  ctx.putImageData(imgData, 0, 0, 0, 165, 1000, 100);
-  ctx.putImageData(imgData, 0, 0, 0, 400, 1000, 50);
-  // for seperate function of draw image only on canoe deck use   ctx.putImageData(imgData, 0, 0, 0, 165, 1000, 100);   ctx.putImageData(imgData, 0, 0, 0, 400, 1000, 50), first for top hull, second for side hull
-  // ctx.drawImage(img, 50, 0,);
-}
-
-function changeBlue() {
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 0;
-    imgData.data[i+1] = 0;
-    imgData.data[i+2] = 255;
-    imgData.data[i+3] = 255;
+  if(placement == "Deck"){
+    ctx.putImageData(imgData, 0, 0, 0, 165, 1000, 100);
+    ctx.putImageData(imgData, 0, 0, 0, 400, 1000, 50);
+  }
+  else if(placement == "Hull"){
+    ctx.putImageData(imgData, 0, 0, 0, 450, 1000, 75);
+  }
+  else if(placement == "Ama"){
+    ctx.putImageData(imgData, 0, 0, 0, 0, 1000, 59);
+    ctx.putImageData(imgData, 0, 0, 0, 300, 1000, 80);
   };
-  ctx.putImageData(imgData, 0, 0, 0, 165, 1000, 100);
-  ctx.putImageData(imgData, 0, 0, 0, 400, 1000, 50);
 }
 
-function changeGreen() {
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 0;
-    imgData.data[i+1] = 255;
-    imgData.data[i+2] = 0;
-    imgData.data[i+3] = 255;
-  };
-  ctx.putImageData(imgData, 0, 0, 0, 165, 1000, 100);
-  ctx.putImageData(imgData, 0, 0, 0, 400, 1000, 50);
-}
-
-function changeWhite() {
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 255;
-    imgData.data[i+1] = 255;
-    imgData.data[i+2] = 255;
-    imgData.data[i+3] = 255;
-  };
-  ctx.putImageData(imgData, 0, 0, 0, 165, 1000, 100);
-  ctx.putImageData(imgData, 0, 0, 0, 400, 1000, 50);
-}
-
-
-function changeRedHull() {
-  var x = event.clientX;
-  var y = event.clientY; 
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 255;
-    imgData.data[i+1] = 0;
-    imgData.data[i+2] = 0;
-    imgData.data[i+3] = 255;
-  };
-  ctx.putImageData(imgData, 0, 0, 0, 450, 1000, 75);
-  // for seperate function of draw image only on canoe deck use   ctx.putImageData(imgData, 0, 0, 0, 165, 1000, 100);   ctx.putImageData(imgData, 0, 0, 0, 400, 1000, 50), first for top hull, second for side hull
-  // ctx.drawImage(img, 50, 0,);
-}
-
-function changeBlueHull() {
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 0;
-    imgData.data[i+1] = 0;
-    imgData.data[i+2] = 255;
-    imgData.data[i+3] = 255;
-  };
-  ctx.putImageData(imgData, 0, 0, 0, 450, 1000, 75);
-  // ctx.drawImage(img, 50, 0);
-}
-
-function changeGreenHull() {
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 0;
-    imgData.data[i+1] = 255;
-    imgData.data[i+2] = 0;
-    imgData.data[i+3] = 255;
-  };
-  ctx.putImageData(imgData, 0, 0, 0, 450, 1000, 75);
-  // ctx.drawImage(img, 50, 0);
-}
-
-function changeWhiteHull() {
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 255;
-    imgData.data[i+1] = 255;
-    imgData.data[i+2] = 255;
-    imgData.data[i+3] = 255;
-  };
-  ctx.putImageData(imgData, 0, 0, 0, 450, 1000, 75);
-  // ctx.drawImage(img, 50, 0);
-}
-
-
-function changeRedAma() {
-  var x = event.clientX;
-  var y = event.clientY; 
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 255;
-    imgData.data[i+1] = 0;
-    imgData.data[i+2] = 0;
-    imgData.data[i+3] = 255;
-  };
-  ctx.putImageData(imgData, 0, 0, 0, 0, 1000, 59);
-  ctx.putImageData(imgData, 0, 0, 0, 300, 1000, 80);
-  // for seperate function of draw image only on canoe deck use   ctx.putImageData(imgData, 0, 0, 0, 165, 1000, 100);   ctx.putImageData(imgData, 0, 0, 0, 400, 1000, 50), first for top hull, second for side hull
-  // ctx.drawImage(img, 50, 0,);
-}
-//59-165 for iako
-
-function changeBlueAma() {
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 0;
-    imgData.data[i+1] = 0;
-    imgData.data[i+2] = 255;
-    imgData.data[i+3] = 255;
-  };
-  ctx.putImageData(imgData, 0, 0, 0, 0, 1000, 59);
-  ctx.putImageData(imgData, 0, 0, 0, 300, 1000, 80);
-  // ctx.drawImage(img, 50, 0);
-}
-
-function changeGreenAma() {
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 0;
-    imgData.data[i+1] = 255;
-    imgData.data[i+2] = 0;
-    imgData.data[i+3] = 255;
-  };
-  ctx.putImageData(imgData, 0, 0, 0, 0, 1000, 59);
-  ctx.putImageData(imgData, 0, 0, 0, 300, 1000, 80);
-  // ctx.drawImage(img, 50, 0);
-}
-
-function changeWhiteAma() {
-  var imgData = ctx.getImageData(0, 0, c.width, c.height);
-  // invert colors
-  var i;
-  for (i = 0; i < imgData.data.length; i += 4) {
-    imgData.data[i] = 255;
-    imgData.data[i+1] = 255;
-    imgData.data[i+2] = 255;
-    imgData.data[i+3] = 255;
-  };
-  ctx.putImageData(imgData, 0, 0, 0, 0, 1000, 59);
-  ctx.putImageData(imgData, 0, 0, 0, 300, 1000, 80);
-  // ctx.drawImage(img, 50, 0);
-}
 
 function changeSticker() {
   // var imageHTML = document.getElementById("tribalSticker")
@@ -259,32 +104,3 @@ function changeSticker() {
   // img.style.transform = "rotate(95deg)";
   // img.style.POSITION = "absolute";
 }
-
-// function singleClick() {
-//   // ctx.drawImage(img, 50, 0);
-//     var imgData = ctx.getImageData(0, 0, c.width, c.height);
-//     // invert colors
-//     var i;
-//     for (i = 0; i < imgData.data.length; i += 4) {
-//       imgData.data[i] = 255;
-//       imgData.data[i+1] = 0;
-//       imgData.data[i+2] = 0;
-//       imgData.data[i+3] = 255;
-//     };
-//     ctx.putImageData(imgData, 0, 0);
-//   // ctx.drawImage(img, 50, 0);
-// };
-
-// function colorInvert() {
-//   ctx.drawImage(img, 50, 0);
-//   var imgData = ctx.getImageData(0, 0, c.width, c.height);
-//   // invert colors
-//   var i;
-//   for (i = 0; i < imgData.data.length; i += 4) {
-//     imgData.data[i] = 255 - imgData.data[i];
-//     imgData.data[i+1] = 255 - imgData.data[i+1];
-//     imgData.data[i+2] = 255 - imgData.data[i+2];
-//     imgData.data[i+3] = 255;
-//   };
-//   ctx.putImageData(imgData, 0, 0);
-// }
